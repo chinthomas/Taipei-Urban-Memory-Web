fd8e857e95309ecc2a177a70f25eba97890877df
chinthomas <chengyou928@gmail.com>
2024-12-09 22:54:33 +0800

    add icon legend on journalPage

--- src/PageJournal.jsx
+++ src/PageJournal.jsx
@@ -1,8 +1,24 @@
 import Timeline from "./Timeline";
+import governmentIcon from "./assets/government.svg"
+import residentsIcon from "./assets/residents.svg"
+import graduateIcon from "./assets/graduate.svg"
 function PageJournal(){
   return(
     <>
-        <h1>Journal</h1>
+      <div className="icon-legend">
+        <div>
+          <img className="icon" src={governmentIcon} alt="gov" id="gov"/>
+          <label htmlFor="gov">政府</label>
+        </div>
+        <div>
+          <img src={residentsIcon} alt="residents" id="residents"/>        
+          <label htmlFor="residents">居民</label>
+        </div>
+        <div>
+          <img src={graduateIcon} alt="graduate" id="graduate"/>        
+          <label htmlFor="graduate">城鄉所</label>
+        </div>
+      </div>
         <Timeline />
     </>
 
